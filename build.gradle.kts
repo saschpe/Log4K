7518cf3bd67dcef68bf5bb445d435099067785c9
Sascha Peilicke <sascha@peilicke.de>
2020-06-14 13:21:46 +0200

    Gradle: Update versions plugin to 0.28.0
    See https://github.com/ben-manes/gradle-versions-plugin/releases/tag/v0.28.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 plugins {
     id("com.diffplug.gradle.spotless") version "3.27.1"
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.github.ben-manes.versions") version "0.28.0"
     kotlin("jvm") version "1.3.72"
 }
 
