4a0c4c3b10440376773306336d154494b2b697ee
Sascha Peilicke <sascha@peilicke.de>
2020-10-28 11:21:47 +0100

    Gradle: Update versions plugin to 0.33.0
    See https://github.com/ben-manes/gradle-versions-plugin/releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 plugins {
     id("com.diffplug.gradle.spotless") version "4.3.0"
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("com.github.ben-manes.versions") version "0.33.0"
     kotlin("jvm") version "1.4.10"
 }
 
