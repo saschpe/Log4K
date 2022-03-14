5fb627bb978004a75f78c3960a6a655d9b2a93e4
Sascha Peilicke <sascha@peilicke.de>
2022-03-14 15:31:51 +0100

    Update to most recent Kotlin/Multiplatform
    Based on Kotlin 1.6.20-RC. Simplify dependenciy on kotlin-test library.
    Add support for iOS Simulator on Apple M1 hardware. Remove custom iOS
    Framework generation now that Kotlin/Multiplatform Gradle plugin does it
    on it's own.

--- log4k/build.gradle.kts
+++ log4k/build.gradle.kts
@@ -10,136 +10,46 @@ repositories {
     google()
 }
 
-android {
-    compileSdkVersion(30)
-
-    defaultConfig {
-        minSdkVersion(17)
-        targetSdkVersion(30)
-    }
-
-    sourceSets {
-        named("main") {
-            manifest.srcFile("src/androidMain/AndroidManifest.xml")
-            java.srcDirs("src/androidMain/kotlin")
-            res.srcDirs("src/androidMain/res")
-        }
-    }
-
-    lintOptions {
-        isCheckReleaseBuilds = false
-        isAbortOnError = false
-    }
-
-    testOptions.unitTests.isIncludeAndroidResources = true
-}
-
-group = "de.peilicke.sascha"
-version = "1.0.1"
-
 kotlin {
-    android {
-        compilations.all { kotlinOptions.jvmTarget = "1.8" }
-        publishAllLibraryVariants()
-    }
-    iosArm64 { binaries.framework("Log4K") }
-    iosX64 { binaries.framework("Log4K") }
+    android { publishAllLibraryVariants() }
+    ios { binaries.framework("Log4K") }
+    iosSimulatorArm64 { binaries.framework("Log4K") }
     js {
-        browser()
+//        nodejs() // TODO: Wait for Kotlin-1.6.20 which uses Node-16 instead of 14.
         compilations.all {
             kotlinOptions.sourceMap = true
             kotlinOptions.moduleKind = "umd"
         }
     }
-    jvm { compilations.all { kotlinOptions.jvmTarget = "1.8" } }
+    jvm { testRuns["test"].executionTask.configure { useJUnitPlatform() } }
 
-    sourceSets {
-        named("commonMain") { }
-        named("commonTest") {
-            dependencies {
-                implementation(kotlin("test-annotations-common"))
-                implementation(kotlin("test-common"))
-                implementation("io.mockk:mockk-common:1.11.0")
-            }
-        }
-
-        named("androidMain") {
-        }
-        named("androidTest") {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
-                implementation("androidx.test:core-ktx:1.3.0")
-                implementation("androidx.test.ext:junit-ktx:1.1.2")
-                implementation("io.mockk:mockk:1.11.0")
-            }
-        }
-
-        val iosMain by creating {
-            dependencies { }
-        }
-        val iosTest by creating {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        getByName("iosArm64Main") { dependsOn(iosMain) }
-        getByName("iosArm64Test") { dependsOn(iosTest) }
-        getByName("iosX64Main") { dependsOn(iosMain) }
-        getByName("iosX64Test") { dependsOn(iosTest) }
-
-        named("jsMain") { }
-        // Note: mockk is not available for JavaScript
-        named("jsTest") {
-            dependencies {
-                implementation(kotlin("test-js"))
-                implementation("io.mockk:mockk-dsl-js:1.11.0")
-            }
-        }
-
-        named("jvmMain") { }
-        named("jvmTest") {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
-                implementation("io.mockk:mockk:1.11.0")
-            }
-        }
+    sourceSets["commonTest"].dependencies {
+        implementation(kotlin("test"))
     }
+    sourceSets["iosSimulatorArm64Main"].dependsOn(sourceSets["iosMain"])
+    sourceSets["iosSimulatorArm64Test"].dependsOn(sourceSets["iosTest"])
 
-    tasks {
-        register("universalFrameworkDebug", org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask::class) {
-            baseName = "Log4K"
-            from(
-                iosArm64().binaries.getFramework("Log4K", "Debug"),
-                iosX64().binaries.getFramework("Log4K", "Debug")
-            )
-            destinationDir = buildDir.resolve("bin/universal/debug")
-            group = "Universal framework"
-            description = "Builds a universal (fat) debug framework"
-            dependsOn("linkLog4KDebugFrameworkIosArm64")
-            dependsOn("linkLog4KDebugFrameworkIosX64")
-        }
+    sourceSets.remove(sourceSets["androidAndroidTestRelease"]) // https://issuetracker.google.com/issues/152187160
 
-        register("universalFrameworkRelease", org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask::class) {
-            baseName = "Log4K"
-            from(
-                iosArm64().binaries.getFramework("Log4K", "Release"),
-                iosX64().binaries.getFramework("Log4K", "Release")
-            )
-            destinationDir = buildDir.resolve("bin/universal/release")
-            group = "Universal framework"
-            description = "Builds a universal (fat) release framework"
-            dependsOn("linkLog4KReleaseFrameworkIosArm64")
-            dependsOn("linkLog4KReleaseFrameworkIosX64")
-        }
-
-        register("universalFramework") {
-            dependsOn("universalFrameworkDebug")
-            dependsOn("universalFrameworkRelease")
-        }
+    targets.withType(org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithSimulatorTests::class.java) {
+        testRuns["test"].deviceId = "iPhone 13"
     }
 }
+
+android {
+    buildToolsVersion = "32.0.0"
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 17
+        targetSdk = 31
+    }
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+}
+
+group = "de.peilicke.sascha"
+version = "1.0.1"
 
 val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
