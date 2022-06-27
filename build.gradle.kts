a50616f5fb1b17c80e01a2dff72758130aa0e5e9
Sascha Peilicke <sascha@peilicke.de>
2022-06-27 09:49:50 +0200

    Android: Update to API 32
    And use Android Gradle Plugin 7.2.1 as well as build-tools 33.0.0.
    See upstream release notes:
    
     - https://developer.android.com/studio/releases/platforms#12
     - https://developer.android.com/studio/releases/gradle-plugin#7-2-0
     - https://developer.android.com/studio/releases/build-tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.7.0"
-    id("com.android.library") version "7.0.4" apply false
+    id("com.android.library") version "7.2.1" apply false
     id("com.diffplug.spotless") version "6.7.2"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
