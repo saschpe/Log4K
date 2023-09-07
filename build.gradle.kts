2035b6abab06036755fe83c4d94fd8f546b4a8ef
Sascha Peilicke <sascha@peilicke.de>
2023-09-07 20:13:59 +0200

    Gradle: Update Spotless plugin to 6.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,20 @@
 plugins {
     kotlin("jvm") version "1.9.10"
     id("com.android.library") version "7.3.0" apply false
-    id("com.diffplug.spotless") version "6.11.0"
+    id("com.diffplug.spotless") version "6.21.0"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 spotless {
-    format("misc") {
-        target("**/*.gradle", "*.md", "**/.gitignore")
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
     freshmark {
-        target("*.md")
+        target("**/*.md")
         propertiesFile("gradle.properties")
     }
     kotlin {
-        target("source/**/src/**/*.kt")
-        targetExclude("**/build/**/*.kt")
-        ktlint().editorConfigOverride(
-            mapOf("disabled_rules" to "filename,no-wildcard-imports", "insert_final_newline" to false)
-        )
+        ktlint()
     }
     kotlinGradle {
-        target("**/*.gradle.kts")
-        ktlint().editorConfigOverride(mapOf("insert_final_newline" to false))
+        ktlint()
     }
 }
 
