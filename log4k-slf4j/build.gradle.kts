ea6916bf2c51d0e0f015a81bcdb51518d1830920
Sascha Peilicke <sascha@peilicke.de>
2025-09-23 16:17:40 +0200

    Implement Maven Central publishing with a Gradle community plugin
    See: https://github.com/vanniktech/gradle-maven-publish-plugin

--- log4k-slf4j/build.gradle.kts
+++ log4k-slf4j/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    `maven-publish`
     alias(libs.plugins.android.library)
     alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.multiplatform)
-    signing
+    alias(libs.plugins.maven.publish)
 }
 
 kotlin {
@@ -38,64 +37,34 @@ android {
     testCoverage.jacocoVersion = libs.versions.jacoco.get()
 }
 
-publishing {
-    publications.withType<MavenPublication> {
-        groupId = "de.peilicke.sascha"
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 
-        artifact(project.tasks.register("${name}DokkaJar", Jar::class) {
-            group = JavaBasePlugin.DOCUMENTATION_GROUP
-            description = "Assembles Kotlin docs with Dokka into a Javadoc jar"
-            archiveClassifier.set("javadoc")
-            from(tasks.named("dokkaGeneratePublicationHtml"))
-            archiveBaseName.set("${archiveBaseName.get()}-$name")
-        })
+    coordinates("de.peilicke.sascha", name, version.toString())
 
-        pom {
-            name.set("Log4K-SLF4J")
-            description.set("Lightweight logging library for Kotlin/Multiplatform - SLF4J integration. Supports Android, iOS, JavaScript and plain JVM environments.")
+    pom {
+        name.set("Log4K-SLF4J")
+        description.set("Lightweight logging library for Kotlin/Multiplatform - SLF4J integration. Supports Android, iOS, JavaScript and plain JVM environments.")
+        url.set("https://github.com/saschpe/log4k")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("saschpe")
+                name.set("Sascha Peilicke")
+                email.set("sascha@peilicke.de")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/saschpe/log4k.git")
+            developerConnection.set("scm:git:ssh://github.com/saschpe/log4k.git")
             url.set("https://github.com/saschpe/log4k")
-
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/licenses/MIT")
-                }
-            }
-            developers {
-                developer {
-                    id.set("saschpe")
-                    name.set("Sascha Peilicke")
-                    email.set("sascha@peilicke.de")
-                }
-            }
-            scm {
-                connection.set("scm:git:git://github.com/saschpe/log4k.git")
-                developerConnection.set("scm:git:ssh://github.com/saschpe/log4k.git")
-                url.set("https://github.com/saschpe/log4k")
-            }
         }
     }
-
-    if (hasProperty("sonatypeUser") && hasProperty("sonatypePass")) {
-        repositories {
-            maven {
-                name = "sonatype"
-                credentials {
-                    username = property("sonatypeUser") as String
-                    password = property("sonatypePass") as String
-                }
-                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-            }
-        }
-    }
-}
-
-signing {
-    val sonatypeGpgKey = System.getenv("SONATYPE_GPG_KEY")
-    val sonatypeGpgKeyPassword = System.getenv("SONATYPE_GPG_KEY_PASSWORD")
-    when {
-        sonatypeGpgKey == null || sonatypeGpgKeyPassword == null -> useGpgCmd()
-        else -> useInMemoryPgpKeys(sonatypeGpgKey, sonatypeGpgKeyPassword)
-    }
-    sign(publishing.publications)
 }
